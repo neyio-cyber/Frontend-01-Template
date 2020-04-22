d41272e23921cc19bcb1aa5b5dc77f874a31d9e8
Neyio <neyioqian@gmail.com>
2020-04-22 20:21:10 +0800

    判断是否为数字

--- week02/isNumber.js
+++ week02/isNumber.js
@@ -1,26 +1,52 @@
 const isNumber = (num) =>
-  /^(\-|\+)?(([1-9]\d*\.?\d*|0\.?\d*[1-9]\d)|0x(([1-9a-fA-F][\da-fA-F]*|0)\.?[\da-fA-F]*)|(0b(([1][0-1]*)|0)\.?[0-1]*)|(0o(([1-7][0-7]*)|0)\.?[0-7]*))$/.test(
+  /^[-\+]?(([1-9]\d*\.?\d*|0\.?\d*[1-9]\d)|([1-9]\d*\.?\d*([eE](\-)?)?\d*|0)|^[-\+]?0[xX](([1-9a-fA-F][\da-fA-F]*|0))|[-\+]?0[bB]([0-1]*|0)*|[-\+]?0[Oo]([0-7]*|0))$/.test(
     num
   );
 
-console.log(isNumber("10.123")); //true
-console.log(isNumber("-10.123")); //true
-console.log(isNumber("-10")); //true
-console.log("-10.123g", isNumber("-10.123g")); //false
-console.log(isNumber("-0x123456789abcdef")); //true
-console.log(isNumber("0x123456789abcdef")); //true
-console.log(isNumber("0x123456789abcdef.acf")); //true
-console.log("0x123456789abcdef.acg", isNumber("0x123456789abcdef.acg")); //false
-console.log("0xs12", isNumber("0xs12")); //false a-f之间
-console.log(isNumber("0b0")); //true
-console.log(isNumber("0b10")); //true
-console.log("0b20", isNumber("0b20")); //false 0-1
-console.log(isNumber("0b10.0110")); //true
-console.log("0b10.0120", isNumber("0b10.0120")); //false
-console.log(isNumber("0o10")); //true
-console.log(isNumber("-0o0")); //true
-console.log(isNumber("-0o10")); //true
-console.log(isNumber("0o12345670")); //true
-console.log(isNumber("0o123456780")); //false 0-7
-console.log("0o123456780", isNumber("0o123456780")); //false 0-7
-console.log("0o1234567.12345678", isNumber("0o1234567.12345678")); //false 0-7
+const isNumber2 = (num) =>
+  /^[-\+]?(\.\d+|(0|[1-9]\d*)\.?\d*?)([eE][-\+]?\d+)?$|^[-\+]?0[bB][01]+$|^[-\+]?0[oO][0-7]+$|^[-\+]?0[xX][0-9a-fA-F]+$/.test(
+    num
+  );
+
+const cases = [
+  "10.123",
+  "010.123", //false
+  "10.1e10",
+  "10.1E10",
+  "10e-1",
+  "-10e-1",
+  "10e--1", //false
+  "-10.123g",
+  "-0x123456789abcdef",
+  "0X123456789abcdef",
+  "0x123456789abcdef.acf",
+  "0xs12",
+  "0b0",
+  "0b10",
+  "-0b10",
+  "0b20",
+  "0o10",
+  "-0o01",
+  "0o123456780",
+];
+
+const test = (fn) => (arrs) =>
+  arrs.reduce(
+    (pre, cur) => ({
+      ...pre,
+      [cur]: fn(cur),
+    }),
+    {}
+  );
+
+const case1 = test(isNumber)(cases);
+const case2 = test(isNumber2)(cases);
+const matched = Object.entries(case1).every(([k, v]) => {
+  if (v !== case2[k]) {
+    console.log(k);
+  }
+  return v === case2[k];
+});
+
+console.log("matched", matched);
+console.log(case2);
